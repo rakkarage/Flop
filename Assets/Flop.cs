6b15d49ca1d7fd4bd1e890a298de1ce04013013f
rakkarage <rakkarage@gmail.com>
2014-09-03 09:39:50 -0400

    Reorder

--- Assets/Flop.cs
+++ Assets/Flop.cs
@@ -1,41 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.EventSystems;
-public class Flop : UIBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class Flop : UIBehaviour, IDragHandler
 {
 	public float Offset = 64f;
 	public Transform LookAt;
 	public Transform Content;
+	public bool Reorder = false;
 	protected override void Start()
 	{
 		for (int i = 0; i < Content.childCount; i++)
 		{
 			var x = i * Offset;
-			Pan(x, Content.GetChild(i));
+			Drag(x, Content.GetChild(i));
 		}
 	}
-	public void Pan(PointerEventData e)
+	public void Drag(PointerEventData e)
 	{
 		foreach (Transform i in Content)
 		{
 			var x = i.position.x + e.delta.x;
-			Pan(x, i);
+			Drag(x, i);
+		}
+		if (Reorder)
+		{
+			var d = new Dictionary<int, Vector3>();
+			for (int i = 0; i < Content.childCount; i++)
+			{
+				var child = Content.GetChild(i);
+				d.Add(i, child.localPosition);
+			}
+			var order = 0;
+			foreach (KeyValuePair<int, Vector3> i in d.OrderBy(key => Mathf.Abs(key.Value.x)))
+			{
+				Content.GetChild(i.Key).SetSiblingIndex(d.Count - order++);
+			}
 		}
 	}
-	private void Pan(float x, Transform t)
+	private void Drag(float x, Transform t)
 	{
 		t.position = new Vector3(x, t.position.y, x < 0 ? -x : x);
 		t.rotation = Quaternion.LookRotation(LookAt.position - t.position);
 	}
-	public void OnBeginDrag(PointerEventData e)
-	{
-		Pan(e);
-	}
 	public void OnDrag(PointerEventData e)
 	{
-		Pan(e);
-	}
-	public void OnEndDrag(PointerEventData e)
-	{
-		Pan(e);
+		Drag(e);
 	}
 }
